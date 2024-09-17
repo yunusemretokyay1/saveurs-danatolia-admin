b11008bef00bfa950235fd5f85f37bb098cec62c
yunusemretokyay1 <yunusemretokyay@gmail.com>
2024-09-17 10:52:37 +0200

    testing

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    S3_SECRET_ACCESS_KEY: process.env.S3_SECRET_ACCESS_KEY,
+    S3_ACCESS_KEY: process.env.S3_ACCESS_KEY,
+    MONGODB_URI: process.env.MONGODB_URI,
+    GOOGLE_ID: process.env.GOOGLE_ID,
+    GOOGLE_SECRET: process.env.GOOGLE_SECRET,
+  },
 };
 
 export default nextConfig;
